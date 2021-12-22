1e0f60b4ba4968733962e6967bebdd9c88fd8aa9
Mateusz_Palczuk <mateusz.palczuk@gmail.com>
2021-12-22 19:27:27 +0100

    tried to do sth, but it doesnt work

--- cw5.py
+++ cw5.py
@@ -1,6 +1,7 @@
 import csv
 import math
 import numpy as np
+from numpy.core.fromnumeric import size
 
 # returns list of tuples as data with onli the data and header is one tuple with names of elements in tuples
 def readFile(file_name):
@@ -58,15 +59,74 @@ def getDList(data):
             D.append(item[d_idx])
     return D
 
+# class doing all neural network calculations and storing its data
 class NeuralNetwork:
 
-    def __init__(self, learn_data):
-        pass
+    def __init__(self, learn_data, input_num, hidden_num, output_num):
+        d_idx = getDIdx(learn_data)
+        learn_data = np.array(learn_data)
+        self.inputs = learn_data[:, :d_idx]
+        self.outputs_correct = learn_data[:, d_idx]
+        size = np.size(self.inputs, 1)
+        self.weights1 = np.ones((input_num, hidden_num)) * 0.5
+        self.weights2 = np.ones((hidden_num, output_num)) * 0.5
+        self.errors = []
+
+    # calculates value of sigmoid function
+    def sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
+
+    # calculates value of derivative of sigmoid function
+    def sigmoidDer(self, x):
+        return x * (1 - x)
+
+    # calculates values of hidden and output layers
+    def feedForward(self):
+        self.hidden = self.sigmoid(np.dot(self.inputs, self.weights1))
+        self.outputs = self.sigmoid(np.dot(self.hidden, self.weights2))
+
+    # calculates error and adds it to errors list
+    def calculateError(self):
+        a = self.outputs - self.outputs_correct
+        self.errors.append(np.dot(a, a))
+
+    # does whole back propagation (idk how it works)
+    def backPropagation(self):
+        d_weights2 = np.dot(self.hidden.T, (2 * (self.outputs_correct - self.outputs) * self.sigmoidDer(self.outputs)))
+        d_weights1 = np.dot(self.inputs.T, (np.dot(2 * (self.outputs_correct - self.outputs) \
+            * self.sigmoidDer(self.outputs), self.weights2.T) * self.sigmoidDer(self.hidden)))
+        self.weights1 += d_weights1
+        self.weights2 += d_weights2
+
+    # trains the network over 'iter' iterations
+    def train(self, iter):
+        for i in range(iter):
+            self.feedForward()
+            self.backPropagation()
+            self.calculateError()
+
+# tests neural network
+def test(filename, coef):
+    # imports data from file
+    data, header = readFile(filename)
+    data = convertDataToFloat(data)
+    learn_data, test_data = divideLearnTestData(data, coef)
+    # tests some basics
+    learn_data = np.array(
+        [[2, 3, 4, 5],
+        [534, 12, 34, 678],
+        [7, 7, 12, 78],
+        [45, 678, 12, 37],
+        [345, 678, 12, 56]]
+    )
+    NN = NeuralNetwork(learn_data, 3, 4, 1)
+    NN.train(10)
+    print(NN.errors)
 
 def main():
     red = 'winequality-red.csv'
     white = 'winequality-white.csv'
-    data, header = readFile(red)
+    test(red, 0.6)
 
 if __name__ == '__main__':
     main()
